60d316819944e15d4f00e34f9e1cd1896ff0ec1a
Ryusei Takeda <illyasviel-hercules@au.com>
2020-11-18 16:23:12 +0000

    add a file

---
+++ 3.bash
@@ -0,0 +1,26 @@
+linux torvalds氏の代表的なソフト
+　・linux
+　・git
+
+
+1:git上にリポジトリを作成
+　・右上の「New」をクリック
+　・リポジトリ名、その他設定
+
+2:リポジトリを取得する
+　・CodeからURLを取得
+　・リポジトリを作りたいディレクトリで"git clone(URL)"と入力
+
+3:リポジトリにコードの追加
+　・"git add (追加したいファイル名)"
+　・”git status”
+　・"git commit -m"適当に何かコメント""
+
+4:masterとブランチ
+　・元と,分岐後のver
+
+5:ブランチ作成
+　・git checkout -b (ブランチ名)
+※git branch で確認可能
+※git merge (ブランチ名)でmasterに更新
+
